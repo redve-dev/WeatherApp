c0c8686c43bb79123b4fb2b92e9d51363bbf5cd0
Beniamin <106469071+redve-dev@users.noreply.github.com>
2022-07-05 10:44:23 +0200

    small refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include "../lib/functions.hpp"
 #include <cassert>
+#include <algorithm>
+#include <cassert>
 
 int main(){
 	const std::string apikey = GetAPIKey();
-	const std::string request = GetRequest();
-	assert(!(apikey.empty() || request.empty()));
-	const std::string result = PerformRequest(request+apikey);
-	std::cout<<result<<std::endl;
+	assert(!apikey.empty());
+	const std::string request = InsertDataToRequest(GetRequest(), "katowice", apikey);
+	assert(!request.empty());
+	std::string result = PerformRequest(request);
+	std::replace(result.begin(), result.end(), ' ', '_');
+	std::cout<<result;
 	return 0;
 }
